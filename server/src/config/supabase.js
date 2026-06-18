950ef247e905290b78e81af5dc25c94e8998b463
camposdevs <devcampos2007@gmail.com>
2026-06-18 10:34:30 -0300

    fix: adicionando ws para supabase

--- server/src/config/supabase.js
+++ server/src/config/supabase.js
@@ -1,9 +1,15 @@
 const { createClient } = require("@supabase/supabase-js");
+const WebSocket = require("ws");
 const env = require("./env");
 
 const supabase = createClient(
   env.supabaseUrl,
-  env.supabaseServiceRoleKey
+  env.supabaseServiceRoleKey,
+  {
+    realtime: {
+      transport: WebSocket
+    }
+  }
 );
 
 module.exports = supabase;
